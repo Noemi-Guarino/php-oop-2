3fcf7b6e5cabd08fb0c808eed1f84f6187f51619
Noemi-Guarino <noemi.guarino.1996@gmail.com>
2024-02-19 16:14:05 +0100

    edit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require __DIR__.('product.php');
+require __DIR__.('categories.php');
+require __DIR__.('food.php');
+require __DIR__.('card.php');
+require __DIR__.('toys.php');
+require __DIR__.('kennels.php');
